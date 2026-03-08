4876a0a2c6f722d8e42f763a00984af733e817d4
alebaqinduni <alebaqinduni@gmail.com>
2026-03-08 05:23:42 +0500

    Added new files

---
+++ lab/week2/color.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "\033[36m";
+    cout << "\033[46m";
+    cout<<"      =================This is a color================"<<endl;
+    cout << "\033[0m";
+}
